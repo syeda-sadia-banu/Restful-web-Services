92382fb879e8b6dc9f5cb8b25c4008a8f1e543de
syeda-sadia-banu <sadia.banu17@gmail.com>
2025-03-28 19:47:19 +0530

    Returning a collection of resources with collectionModel

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/ws/ui/controller/UserController.java
@@ -9,6 +9,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -125,7 +126,7 @@ public class UserController {
 	// http://localhost:8080/mobile-app-ws/users/userId/addresses
 	@GetMapping(path = "/{id}/addresses", produces = { MediaType.APPLICATION_XML_VALUE,
 			MediaType.APPLICATION_JSON_VALUE })
-	public List<AddressesRest> getUserAddresses(@PathVariable String id) {
+	public CollectionModel<AddressesRest> getUserAddresses(@PathVariable String id) {
 
 		List<AddressesRest> returnValue = new ArrayList<>();
 
@@ -139,7 +140,13 @@ public class UserController {
 			returnValue = modelMapper.map(addressesDto, listType);
 
 		}
-		return returnValue;
+		Link userLink=WebMvcLinkBuilder.linkTo(UserController.class)
+                                       .slash(id)
+                                       .withRel("user");
+		Link selfLink=WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(UserController.class).getUserAddresses(id))
+				                       .withSelfRel();		
+
+		return CollectionModel.of(returnValue,userLink,selfLink );
 
 	}
 
